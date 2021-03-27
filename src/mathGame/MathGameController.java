078fd34b00ae218099a7046abd7ea347b754dc61
JessicaLim8 <jessicalim@bell.net>
2021-03-26 22:29:44 -0400

    chnage controller logic

--- src/mathGame/MathGameController.java
+++ src/mathGame/MathGameController.java
@@ -1,8 +1,22 @@
 package mathGame;
 
+import java.awt.EventQueue;
+
 public class MathGameController {
 	
-	
+	public static void main(String[] args) {
+		MathGameState state = new MathGameState();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MathGameUI window = new MathGameUI(state);
+					//window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 	
 	private int randInt() {
 		return (int) (Math.random() * 10);
