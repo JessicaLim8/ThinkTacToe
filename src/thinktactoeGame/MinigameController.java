5af94d5ec6d8c6b4c13607f7069c740ddeb3d541
JessicaLim8 <jessicalim@bell.net>
2021-04-09 20:44:04 -0400

    Loop through all games

--- src/thinktactoeGame/MinigameController.java
+++ src/thinktactoeGame/MinigameController.java
@@ -20,16 +20,18 @@ public class MinigameController extends Thread{
 	private PatternGameController pattern;
 	private GameController gc = GameController.getInstance();
 	private BrickBreakerMain breaker;
+	private int number;
 	
 	private char turn = 'X';
 	
-	public MinigameController(char turn) {
+	public MinigameController(char turn, int num) {
 		this.math = new MathGameController();
 		this.memory = MemoryCardController.getInstance();
 		this.pattern = new PatternGameController();
 		this.breaker = new BrickBreakerMain();
 		this.reaction = new Gui();
 		this.turn = turn;
+		this.number = num;
 		// TODO initialize reaction, Mile's, and brick breaker mini game
 		// 1 - Miles, 2 - Bill, 3 - Vansh, 4 - Jessica, 5 - Jack
 		minigames.put(1, pattern);
@@ -45,15 +47,15 @@ public class MinigameController extends Thread{
 
 
 	public void run() {
-		Random rand = new Random();
-		int number = rand.nextInt(5) + 1;
-		number = 5; // test a specific game
+//		Random rand = new Random();
+//		int number = rand.nextInt(5) + 1;
+//		number = 5; // test a specific game
 		this.minigames.get(number).startGame(this.turn);
 //		this.gc.dropPiece(result);
 	}
 	
 	public static void main(String[] args) {
-		(new MinigameController('X')).start();
+		(new MinigameController('X', 1)).start();
 	}
 
 }
