05f331973afa4f36c1705111dbf39c3863319032
Vanshaj Verma <vermav2@mcmaster.ca>
2021-04-12 15:40:23 -0400

    finished basic exit screen

--- src/exitGame/ExitScreen.java
+++ src/exitGame/ExitScreen.java
@@ -1,14 +1,14 @@
 package exitGame;
 
-import java.awt.Color;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.BorderLayout;
-import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import thinktactoeGame.GameController;
 
 public class ExitScreen {
 	
@@ -27,19 +27,27 @@ public class ExitScreen {
 		exitFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	
-		JButton button = new JButton("Exit Game");
-		button.addActionListener(new ActionListener() {
+		JButton exitButton = new JButton("Exit Game");
+		exitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		button.setBounds(250, 364, 171, 82);
-		exitFrame.getContentPane().add(button);
+		exitButton.setBounds(250, 364, 171, 82);
+		exitFrame.getContentPane().add(exitButton);
 		
-		JButton button_1 = new JButton("Restart Game");
-		button_1.setBounds(250, 255, 171, 82);
-		exitFrame.getContentPane().add(button_1);
+		JButton restartButton = new JButton("Restart Game");
+		restartButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		restartButton.setBounds(250, 255, 171, 82);
+		exitFrame.getContentPane().add(restartButton);
 		
-		exitFrame.setVisible(true);
+
+		JLabel label = new JLabel(GameController.Winner);
+		label.setBounds(236, 137, 197, 107);
+		label.setFont(new Font("Verdana",1,20));
+		exitFrame.getContentPane().add(label);	
 
 		
 	}
